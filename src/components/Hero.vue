6a2c439e5d2114c528f76461b8526410a3cb8b23
husqy06 <luigf306@gmail.com>
2021-09-29 14:12:10 +0200

    Completamento esercizio e bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="jumbotron">
+    <img src="../assets/vue-js-kk.jpg" alt="">
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.jumbotron {
+  width: 100%;
+  height: 80vh;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
